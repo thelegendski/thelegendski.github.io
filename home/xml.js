377660cf7a124106e0a6a4e8be2bae181891a1b1
ski <108815730+thelegendski@users.noreply.github.com>
2023-01-11 05:30:47 -0500

    Update xml.js

--- home/xml.js
+++ home/xml.js
@@ -1,6 +1,11 @@
 const xml = new XMLHttpRequest()
 xml.onreadystatechange = function(){
-    this.readyState === 4 && this.status === 200 && println('it works!')
+    this.readyState === 4 && this.status === 200 && XMLHandler(this)
 }
 xml.open("GET", "/home/main.xml", true)
 xml.send()
+
+const XMLHandler = xml => {
+    const doc = xml.responseXML
+    println(doc.getElementsByTagName('test')[0].childNodes[0].nodeValue)
+}
