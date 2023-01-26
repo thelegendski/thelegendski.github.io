914fe8bbce57c59fc94ea91b0365be5b12eec104
ski <108815730+thelegendski@users.noreply.github.com>
2023-01-25 19:56:24 -0500

    Update audio.js

--- home/audio.js
+++ home/audio.js
@@ -2,5 +2,5 @@ fetch("test.mp3")
     .then(r => r.arrayBuffer())
       .then(buffer => {
         const arr = new Uint8Array(buffer)
-        println(arr.reduce((str, code) => str += String.fromCharCode(code), ''))
+        println(arr.reduce((str, code) => str += String.charCodeAt(code), ''))
       })
